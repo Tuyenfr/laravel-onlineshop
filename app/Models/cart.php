d24c31e61246f2d40876cfc583d9507710ad7c82
Tuyenfr <127860619+Tuyenfr@users.noreply.github.com>
2024-06-11 19:57:22 +0200

    rename Cart.php for production

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -2,38 +2,40 @@
 
 namespace App\Models;
 
-  class Cart{
-
+class Cart
+{
     public $items = null;
     public $totalQty = 0;
     public $totalPrice = 0;
 
-    public function __construct($oldCart) {
-      
-        if($oldCart) {
-          $this->items = $oldCart->items;
-          $this->totalQty = $oldCart->totalQty;
-          $this->totalPrice = $oldCart->totalPrice;
-        }
-      }
+    public function __construct($oldCart)
+    {
 
-    public function add($item, $size, $color, $qty) {
+        if ($oldCart) {
+            $this->items = $oldCart->items;
+            $this->totalQty = $oldCart->totalQty;
+            $this->totalPrice = $oldCart->totalPrice;
+        }
+    }
+
+    public function add($item, $size, $color, $qty)
+    {
 
         $storedItem = [
-          'qty' => 0,
-          'product_id' => 0,
-          'product_name' => $item->p_name,
-          'product_price' => $item->p_current_price,
-          'product_image' => $item->p_featured_photo,
-          'size' => $size,
-          'color' => $color,
-          'item' => $item
+            'qty' => 0,
+            'product_id' => 0,
+            'product_name' => $item->p_name,
+            'product_price' => $item->p_current_price,
+            'product_image' => $item->p_featured_photo,
+            'size' => $size,
+            'color' => $color,
+            'item' => $item,
         ];
 
-        if($this->items) {
-          if(array_key_exists($item->id, $this->items)) {
-              $storedItem = $this->items[$item->id];
-          }
+        if ($this->items) {
+            if (array_key_exists($item->id, $this->items)) {
+                $storedItem = $this->items[$item->id];
+            }
         }
 
         $storedItem['qty'] += $qty;
@@ -44,27 +46,25 @@ namespace App\Models;
         $storedItem['size'] = $size;
         $storedItem['color'] = $color;
         $this->totalQty += $qty;
-        $this->totalPrice += $item->p_current_price*$qty;
+        $this->totalPrice += $item->p_current_price * $qty;
         $this->items[$item->id] = $storedItem;
-        
     }
 
-    public function updateQty($id, $qty) {
+    public function updateQty($id, $qty)
+    {
 
         $this->totalQty -= $this->items[$id]['qty'];
         $this->totalPrice -= $this->items[$id]['product_price'] * $this->items[$id]['qty'];
         $this->items[$id]['qty'] = $qty;
         $this->totalQty += $qty;
         $this->totalPrice += $this->items[$id]['product_price'] * $qty;
-
     }
 
-    public function removeItem($id) {
+    public function removeItem($id)
+    {
 
         $this->totalQty -= $this->items[$id]['qty'];
         $this->totalPrice -= $this->items[$id]['product_price'] * $this->items[$id]['qty'];
         unset($this->items[$id]);
-
     }
-
-  }
+}
